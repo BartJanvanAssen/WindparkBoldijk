92421bcbdfa3855f93fa5cbb5f4b90242c73b99a
Bart Jan van Assen <bartjanvassen@gmail.com>
2025-05-13 15:40:48 +0200

    Hero3 nu goed op mobiel

--- sections/Hero/Hero3.tsx
+++ sections/Hero/Hero3.tsx
@@ -10,8 +10,7 @@ export default function Hero3({
     <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="mx-auto max-w-2xl text-center">
         {/* <p className="text-base/7 font-semibold text-indigo-600">Welkom om onze inloopavond (eerste dinsdag van de maand)</p> */}
-        <h2 className="mt-2 t
-        ext-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">{title}</h2>
+        <h2 className="mt-2 text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">{title}</h2>
         <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
           {subtitel}
         </p>
