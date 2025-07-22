76441d99fb82620d1b719a0816f90e72ec1031f5
Bart Jan van Assen <bartjanvassen@gmail.com>
2025-07-22 20:21:06 +0200

    andere linkjes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,22 @@ const nextConfig = {
         return [
             {
                 source: '/uren',
+                destination: 'https://docs.google.com/forms/d/e/1FAIpQLScBAYySam8sPXG9njmjEo4nWAzGyYTmQTM7vDRh8wo8Vr5o9g/viewform?usp=header',
+                permanent: false
+            },
+            {
+                source: '/uren-epd',
                 destination: 'https://docs.google.com/forms/d/e/1FAIpQLScyhvUM-vfdv6c10OmT0tQ3N90750bt_ocPp0sie6PrsTaMMA/viewform?usp=sharing',
+                permanent: false
+            },
+            {
+                source: '/logboek',
+                destination: 'https://docs.google.com/forms/d/e/1FAIpQLSeBiL875x1msZeZflvjqAmw5ZBIO6R6yXug0Cvno8gF4CMcRw/viewform?usp=header',
+                permanent: false
+            },
+            {
+                source: '/logboek-epd',
+                destination: 'https://forms.gle/5Dk8NQQmmNm3VWnN6',
                 permanent: false
             },
         ]
